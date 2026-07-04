7a4bf849336808506d33c982daae6403ac3c07b6
wzmarketinga-blip <Ywzmarketinga@gmail.com>
2026-07-04 16:52:33 +0530

    Added Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
 import "./index.css";
 import App from "./App";
 
+// Google Analytics Initialize
+ReactGA.initialize("G-L8KLY89C3X");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
